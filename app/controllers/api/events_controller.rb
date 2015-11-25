68016a085a9eb5156c74f411bbd906a6081a5c05
SaiVardhan <saimca03@gmail.com>
2015-11-25 11:01:36 +0530

    added faraday to parse data

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,14 +1,13 @@
-require 'httparty'
-require 'json'
 class Api::EventsController < ApplicationController
-  
-  def get_events
-     response = URI.parse('https://www.eventbriteapi.com/v3/users/me/owned_events/?token=G5GUSXPTSLQVWWUUQMFZ')
-     json = JSON.parse(response)
-   # json = JSON.parse('{"data"=>[{"date" => "2014-08-11 11:00:00","name" => "Test Event","reservation_capacity"=>14 }]}') #string representing your json
-   # json['events'].each do |element|
-    #Event.create(name: element['name']['text'], date: element['start']['utc']) 
-  #end
-end
 
+  def get_events
+    url = "https://www.eventbriteapi.com/v3/users/me/owned_events/?token=G5GUSXPTSLQVWWUUQMFZ"
+    conn = Faraday.new(url: url)
+    response = conn.get
+    @json = JSON.parse(response.body)
+    @
+    Event.create()
+  end
+json.each do |element|
+end
 end
