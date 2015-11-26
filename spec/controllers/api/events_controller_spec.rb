3472597f1c43fba771229892687003a200fe0143
SaiVardhan <saimca03@gmail.com>
2015-11-26 12:09:02 +0530

    added specs for events controller

--- spec/controllers/api/events_controller_spec.rb
+++ spec/controllers/api/events_controller_spec.rb
@@ -1,59 +1,64 @@
 require 'rails_helper'
+require 'json_spec'
 
 RSpec.describe Api::EventsController, type: :controller do
-  let(:response_json) {
-    {"pagination": {"object_count": 1, "page_number": 1, "page_size": 50, "page_count": 1}, "events": [{
-          "name": {"text": "Test","html": "Test"},"description": {
-            "text": "Test event for api ", 
-            "html": "<P>Test event for api<\/P>"
-          }, 
-          "id": "19702702313", 
-          "url": "http://www.eventbrite.com/e/test-tickets-19702702313", 
-          "start": {
-            "timezone": "Asia/Kolkata", 
-            "local": "2015-11-24T19:00:00", 
-            "utc": "2015-11-24T13:30:00Z"
-          }, 
-          "end": {
-            "timezone": "Asia/Kolkata", 
-            "local": "2015-11-24T22:00:00", 
-            "utc": "2015-11-24T16:30:00Z"
-          }, 
-          "created": "2015-11-24T05:46:03Z", 
-          "changed": "2015-11-24T05:46:03Z", 
-          "capacity": 0, 
-          "status": "draft", 
-          "currency": "USD", 
-          "listed": false, 
-          "shareable": false, 
-          "invite_only": false, 
-          "online_event": false, 
-          "show_remaining": false, 
-          "tx_time_limit": 480, 
-          "hide_start_date": false, 
-          "locale": "en_US", 
-          "is_locked": false, 
-          "privacy_setting": "unlocked", 
-          "logo_id": nil, 
-          "organizer_id": "8765107246", 
-          "venue_id": "12122268", 
-          "category_id": nil, 
-          "subcategory_id": nil, 
-          "format_id": nil, 
-          "resource_uri": "https://www.eventbriteapi.com/v3/events/19702702313/", 
-          "logo": nil
-        }
-      ]
-    }
-  }
 
+  let(:response_json) {{
+    "pagination": {
+        "object_count": 1, 
+        "page_number": 1, 
+        "page_size": 50, 
+        "page_count": 1
+    }, 
+    "events": [
+        {
+            "name": {
+                "text": "Test", 
+                "html": "Test"
+            }, 
+            "description": {
+                "text": "Test event for api ", 
+                "html": "<P>Test event for api<\/P>"
+            }, 
+            "id": "19702702313", 
+            "url": "http://www.eventbrite.com/e/test-tickets-19702702313", 
+            "start": {
+                "timezone": "Asia/Kolkata", 
+                "local": "2015-11-24T19:00:00", 
+                "utc": "2015-11-24T13:30:00Z"
+            }, 
+            "end": {
+                "timezone": "Asia/Kolkata", 
+                "local": "2015-11-24T22:00:00", 
+                "utc": "2015-11-24T16:30:00Z"
+            }, 
+            "created": "2015-11-24T05:46:03Z", 
+            "changed": "2015-11-24T05:46:03Z", 
+            "capacity": 0, 
+            "status": "draft", 
+            "currency": "USD", 
+            "listed": false, 
+            "shareable": false, 
+            "invite_only": false, 
+            "online_event": false, 
+            "show_remaining": false, 
+            "tx_time_limit": 480, 
+            "hide_start_date": false, 
+            "locale": "en_US", 
+            "is_locked": false, 
+            "privacy_setting": "unlocked", 
+            "resource_uri": "https://www.eventbriteapi.com/v3/events/19702702313/"
+        }
+    ]
+}}
   before(:each) do
-    allow_any_instance_of(EventService).to receive(:get_response).and_return(response_json.to_json)
+    #request.headers["accept"] = 'application/json'
+    allow_any_instance_of(EventService).to receive(:get_response).and_return(JSON.parse(response_json.to_json))
   end
 
   describe '#index' do
     it "should render the index page" do
-      get :index
+      get :index#,:format => :json
       expect(response).to render_template :index
     end
 
@@ -67,5 +72,7 @@ RSpec.describe Api::EventsController, type: :controller do
       get :index
       expect(Event.count).to eq 1
     end
-  end
+
+    
+end
 end
